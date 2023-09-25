2eb91bb332c9875712dac6b760cee964efa6f25d
vladChivu <cvb.chivu@gmail.com>
2023-09-25 09:41:13 +0300

    changes

--- src/main/java/cinema/shows/dtos/InputMovieDTO.java
+++ src/main/java/cinema/shows/dtos/InputMovieDTO.java
@@ -1,6 +1,8 @@
 package cinema.shows.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +13,7 @@ import java.util.List;
 @Getter @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class InputMovieDTO {
+    @Size(min = 3, message = "String must have at least 3 characters")
     private String title;
     private Double rating;
     private Short minAge;
